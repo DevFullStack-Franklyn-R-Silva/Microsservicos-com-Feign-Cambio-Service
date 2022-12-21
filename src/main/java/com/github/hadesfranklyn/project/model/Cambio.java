0c071860f83c14b91fcd563624b1d3c2dc5c57fe
Franklyn <franklyn1998@live.com>
2022-12-21 15:39:07 -0300

    Adicionando as Annotations do JPA e Integrando o Serviço ao Banco de Dados

--- src/main/java/com/github/hadesfranklyn/project/model/Cambio.java
+++ src/main/java/com/github/hadesfranklyn/project/model/Cambio.java
@@ -3,17 +3,38 @@ package com.github.hadesfranklyn.project.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class Cambio implements Serializable{
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
+@Entity
+public class Cambio implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "from_currency", nullable = false, length = 3)
 	private String from;
+
+	@Column(name = "to_currency", nullable = false, length = 3)
 	private String to;
+
+	@Column(nullable = false)
 	private BigDecimal conversionFactor;
+
+	@Transient
 	private BigDecimal convertedValue;
+
+	@Transient
 	private String environment;
 
+	// constructors
 	public Cambio() {
 		super();
 	}
@@ -29,6 +50,7 @@ public class Cambio implements Serializable{
 		this.environment = environment;
 	}
 
+	// gets and sets
 	public Long getId() {
 		return id;
 	}
